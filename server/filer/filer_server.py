2711a22cfa4609e54a40b4dad682316420d55ff2
Luo Yi <ext.yi.luo@uaes.com>
2018-01-02 20:24:14 +0800

    use raft

---
+++ server/filer/filer_server.py
@@ -0,0 +1,122 @@
+# -*- coding:utf-8 -*-
+
+import os
+import io
+import sys
+import time
+import pickle
+import socket
+import random
+import logging
+
+from xmlrpc.client import ServerProxy
+
+import humanfriendly
+
+from kazoo.client import KazooClient
+from kazoo.exceptions import NodeExistsError
+
+from flask import Flask, request, render_template, send_file
+from werkzeug.utils import secure_filename
+
+# 获取logger实例，如果参数为空则返回root logger
+logger = logging.getLogger()
+
+# 指定logger输出格式
+formatter = logging.Formatter('%(asctime)s %(levelname)-8s: %(message)s')
+
+# 文件日志
+file_handler = logging.FileHandler("volumn.log")
+file_handler.setFormatter(formatter)  # 可以通过setFormatter指定输出格式
+
+# 控制台日志
+console_handler = logging.StreamHandler(sys.stdout)
+console_handler.formatter = formatter  # 也可以直接给formatter赋值
+
+# 为logger添加的日志处理器
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
+
+# 指定日志的最低输出级别，默认为WARN级别
+logger.setLevel(logging.INFO)
+
+zk = KazooClient(hosts='127.0.0.1:2181')
+zk.start()
+
+UPLOAD_FOLDER = 'uploads'
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+act_master_proxy = dict()
+
+fdb = dict()
+if os.path.isfile('fdb'):
+    fdb = pickle.load(open('fdb', 'rb'))
+
+def update_db():
+    pickle.dump(fdb, open('fdb', 'wb'))
+
+@zk.ChildrenWatch('/master')
+def on_master_change(children):
+    act_master_proxy.clear()
+    for child in children:
+        data, _ = zk.get('/master/%s' % child)
+        master = data.decode()
+        act_master_proxy[master] = ServerProxy(master)
+
+def get_master():
+    return random.choice(list(act_master_proxy.values()))
+
+@app.template_filter()
+def format_size(size):
+    return humanfriendly.format_size(size)
+
+@app.route('/')
+def index():
+    return render_template('index.html', files=fdb)
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'GET':
+        return render_template('upload.html')
+    else:
+        master = get_master()
+        file = request.files['file']
+        filename = secure_filename(file.filename)
+
+        data = file.read()
+        size = len(data)
+
+        fid = master.assign_fid()
+        vid, fkey = fid.split(',')
+        volumns = master.find_volumn(int(vid))
+
+        volumn = ServerProxy(random.choice(volumns))
+
+        volumn.store(fid, data)
+
+        fdoc = {'filename': filename, 'fid': fid, 'size': size}
+        fdb[fid] = fdoc
+        update_db()
+
+        return fid
+
+@app.route('/download/<string:fid>')
+def download(fid):
+    vid, fkey = fid.split(',')
+    master = get_master()
+    volumns = master.find_volumn(int(vid))
+
+    volumn = ServerProxy(random.choice(volumns))
+
+    fdoc = fdb[fid]
+
+    filename = fdoc['filename']
+
+    data = volumn.download(fid).data
+
+    return send_file(io.BytesIO(data), attachment_filename=filename)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
--- server/master/master.py
+++ server/master/master.py
@@ -7,38 +7,42 @@ import random
 
 from xmlrpc.client import ServerProxy
 from xmlrpc.server import SimpleXMLRPCServer
+from pysyncobj import SyncObj, SyncObjConf, replicated
+from pysyncobj.batteries import ReplCounter, ReplDict
 
-class Master(object):
+import config
+
+class Master(SyncObj):
 
     _rpc_methods = ['assign_volumn', 'assign_fid', 'find_volumn']
 
     def __init__(self, logger, host, port):
-        self.logger = logger
-        self.host = host
-        self.port = port
-
-        self.db = dict()
-
-        if os.path.isfile('db'):
-            self.db = pickle.load(open('db', 'rb'))
-
-        if 'vdb' not in self.db:
-            self.db['vdb'] = dict()
-            self.db['vid'] = 0
-            self.db['fkey'] = 0
-
-        self.act_vol_serv = dict()
-        self.act_vol_proxy = dict()
+        cfg = SyncObjConf()
+        cfg.fullDumpFile = 'raft.bin'
+        cfg.logCompactionMinTime = 10
+        cfg.useFork = True
 
         self.serv = SimpleXMLRPCServer(
-            (self.host, self.port),
+            (host, port),
             logRequests=True)
 
         for name in self._rpc_methods:
             self.serv.register_function(getattr(self, name))
 
-    def _update_db(self):
-        pickle.dump(self.db, open('db', 'wb'))
+        self.logger = logger
+        self.host = host
+        self.port = port
+
+        self.act_vol_serv = dict()
+        self.act_vol_proxy = dict()
+
+        self.vid = ReplCounter()
+        self.fkey = ReplCounter()
+        self.db = ReplDict()
+        super(Master, self).__init__(config.addr, config.clusters, cfg, consumers=[self.vid, self.fkey, self.db])
+
+    def update_master(self, masters):
+        pass
 
     def update_volumn(self, volumns):
         self.act_vol_serv.clear()
@@ -48,31 +52,26 @@ class Master(object):
             self.act_vol_proxy[volumn[0]] = ServerProxy(volumn[1])
 
     def assign_volumn(self, size):
-        vid = self.db['vid']
-        self.db['vid'] += 1
+        vid = self.vid.inc(sync=True)
 
         vids = random.sample(self.act_vol_serv.keys(), 2)
-        self.db['vdb'][vid] = vids
-
-        self._update_db()
 
         for vvid in vids:
             self.act_vol_proxy[vvid].assign_volumn(vid, size)
 
-        return True
+        self.db.set(vid, vids)
+
+        return vid
 
     def assign_fid(self):
-        vid = random.choice(list(self.db['vdb'].keys()))
-        fkey = self.db['fkey']
-        self.db['fkey'] += 1
-
-        self._update_db()
+        vid = random.choice(list(self.db.keys()))
+        fkey = self.inc_fkey(sync=True)
 
         fid = '%d,%d' % (vid, fkey)
         return fid
 
     def find_volumn(self, vid):
-        vids = self.db['vdb'][vid]
+        vids = self.db[vid]
         addrs = []
 
         for vid in vids:
